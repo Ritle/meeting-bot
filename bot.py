bc5e77f48b484d6bd0fd872eb81b9f1ba310927b
Ritle <coshei1854@mail.ru>
2025-10-10 23:54:12 +0300

    fix_logger

--- bot.py
+++ bot.py
@@ -14,7 +14,10 @@ import time as time_module
 
 
 
-# Настройка логирования
+# Настройка 
+
+logger = logging.getLogger(__name__)
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
